8d9fbe4f84c17eb516705ec5a4bb5ab79e21bbe6
颜泽芳 <838641570@qq.com>
2023-11-30 15:29:25 +0800

    Create index.jsx

---
+++ example/react18demo/src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+let root;
+function render(props) {
+  root = createRoot(document.getElementById('root'));
+  root.render(<App />);
+}
+
+function storeTest(props) {
+  props.onGlobalStateChange((value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev), true);
+  props.setGlobalState({
+    ignore: props.name,
+    user: {
+      name: props.name,
+    },
+  });
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react18] react app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[react18] props from main framework', props);
+  storeTest(props);
+  render(props);
+}
+
+export async function unmount(props) {
+  console.log('[react18] react app will unmount');
+  root.unmount();
+}
